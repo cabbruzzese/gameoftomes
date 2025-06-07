3325995ac5ad016cd1ff7d9e5517e3ec3d2d8a42
cabbruzz <cabbruzz@gmail.com>
2025-06-07 13:23:27 -0400

    Added release folder/script

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -3,14 +3,36 @@ cd h2
 cd ..
 echo "Creating mod directory"
 $gotPath = ".\gamefiles\got"
-if (-not (test-path $gotPath) ) {
-    mkdir $gotPath
-    mkdir "$gotPath\gfx\menu"
+$gotMenuPath = "$gotPath\gfx\menu"
+if (-not (test-path $gotMenuPath) ) {
+    mkdir $gotMenuPath
 } else {
-    echo "$gotPath already exists"
+    echo "$gotMenuPath already exists"
 }
+
 echo "Copying mod files"
-COPY -Path ".\h2\progs.dat" -Destination ".\gamefiles\got\progs.dat" -Force
-COPY -Path ".\assets\gfx\menu\conback.lmp"  -Destination ".\gamefiles\got\gfx\menu\conback.lmp" -Force
-COPY -Path ".\assets\config\autoexec.cfg"  -Destination ".\gamefiles\got\autoexec.cfg" -Force
+COPY -Path ".\h2\progs.dat" -Destination "$gotPath\progs.dat" -Force
+COPY -Path ".\assets\gfx\menu\conback.lmp"  -Destination "$gotMenuPath\conback.lmp" -Force
+COPY -Path ".\assets\config\autoexec.cfg"  -Destination "$gotPath\autoexec.cfg" -Force
+
+echo "Copying release files"
+$releaseFolder = ".\release"
+$releasePath = ".\release\got"
+$releaseMenuPath = "$releasePath\gfx\menu"
+if (-not (test-path $releaseMenuPath) ) {
+    mkdir $releaseMenuPath
+}
+COPY -Path ".\h2\progs.dat" -Destination "$releasePath\progs.dat" -Force
+COPY -Path ".\assets\gfx\menu\conback.lmp"  -Destination "$releaseMenuPath\conback.lmp" -Force
+COPY -Path ".\assets\config\autoexec.cfg"  -Destination "$releasePath\autoexec.cfg" -Force
+
+$zipDest = "$releaseFolder\got.zip"
+$releaseFiles = ".\release\got\*"
+echo "Deleting existing release zip file"
+if (Test-Path $zipDest) {
+    Remove-Item -Path $zipDest -Force
+}
+echo "Creating release file $zipDest"
+Compress-Archive $releaseFiles $zipDest
+
 echo "Complete"
